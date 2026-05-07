f486bd0754259aea6ef4dc171b0f081364d5cbaf
Maxwell0613-user <traviscm0613@gmail.com>
2026-05-07 13:46:33 -0600

    added a bit of javascript

---
+++ home.js
@@ -0,0 +1,6 @@
+function captureInput() {
+// Get the value from the input field
+let userName = document.getElementById("userInput").value;
+// Display the captured value
+alert("Hello, " + userName + "!");
+}
