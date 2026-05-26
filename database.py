1880509153b6778b32a7667fbfffec85920d6ea0
Maxwell0613-user <traviscm0613@gmail.com>
2026-05-26 12:59:37 -0600

    work on database.py

--- database.py
+++ database.py
@@ -1,3 +1,24 @@
 import sqlite3
 
 connection = sqlite3.connect("database")
+cursor = connection.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS books (title TEXT, pages INTEGER, read INTEGER)")
+
+choice = None
+while choice != 5:
+    print("Select an option")
+    print("1) Add a new book")
+    print("2) Delete a book")
+    print("3) Show all Books")
+    print("4) Read a book")
+    print("5) Exit")
+
+    choice = input("> ")
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        pass
