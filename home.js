e7babbfa0c3f46a18ffa9beeb5796626d64d95bd
Maxwell0613-user <traviscm0613@gmail.com>
2026-05-28 15:36:32 -0600

    added firebase js

--- home.js
+++ home.js
@@ -1,20 +1,24 @@
-// ============================
-// FIREBASE CONFIG
-// ============================
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "YOUR_REAL_API_KEY",
-    authDomain: "rocky-mountain-rolls.firebaseapp.com",
-    projectId: "rocky-mountain-rolls",
-    storageBucket: "rocky-mountain-rolls.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyClC6QLVoSF8aSeDr7gS7cwavNWLOCd-fU",
+  authDomain: "rocky-mountain-rolls.firebaseapp.com",
+  projectId: "rocky-mountain-rolls",
+  storageBucket: "rocky-mountain-rolls.firebasestorage.app",
+  messagingSenderId: "953087171248",
+  appId: "1:953087171248:web:5afa477880eb2cf0b2f975",
+  measurementId: "G-X2B2NCHSQ5"
 };
 
-// ============================
-// INITIALIZE FIREBASE (MODULAR)
-// ============================
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 // ============================
 // SAVE EMAIL FUNCTION
