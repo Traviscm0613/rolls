ae028b4c1a4deb676ebdedfcd9adee75bb294692
Maxwell0613-user <traviscm0613@gmail.com>
2026-06-09 15:39:53 -0600

    working on rolls table

--- home.js
+++ home.js
@@ -58,79 +58,29 @@ function saveEmail() {
 
 
 /* form work */
+const form = document.querySelector("#infoForm");
 
 
-const form = document.querySelector("#info_form");
-const travelRange = document.querySelector("#travelRange");
-const notesContainer = document.querySelector("#notesContainer");
-const notes = document.querySelector("#notes");
-const output = document.querySelector("#output");
-const campusBoxes = document.querySelectorAll('input[name="campus"]');
-
-function updateNotesField() {
-  const value = travelRange.value;
-
-  // Show the travel notes on the form if they are choosing many campuses and require it
-  if (value === 'many') {
-    notesContainer.hidden = false;
-    notes.required = true;
-  }
-  else {
-    notesContainer.hidden = true;
-    notes.required = false; 
-  }
-}
-
-travelRange.addEventListener("change", updateNotesField);
-updateNotesField();
-
-
-// Ensure they choose a date later than the current date
-function isPastDate(value) {
-  const today = new Date();
-  const chosen = new Date(value);
-  return chosen < today;
-}
-
-function getSelectedCampuses() {
-  //.from converts a NodeList into a real array, so then you can use .filter and .map
-  return Array.from(campusBoxes)
-    .filter(box => box.checked)
-    .map(box => box.value); 
-}
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   output.textContent = "";
 
-  const firstName = form.firstName.value.trim();
-  const lastName = form.lastName.value.trim();
+  const firstName = form.first.value.trim();
+  const lastName = form.last.value.trim();
   const email = form.email.value.trim();
-  const type = form.travelRange.value;
-  const availableDate = form.availableDate.value;
-  const selectedCampuses = getSelectedCampuses();
-  const note = form.notes.value.trim();
 
-  // Let the user know if they choose many campuses but didn't put a note that they need to add a note
-  if (type == 'many' && !notes) {
-    output.textContent = 'Please add a travel note. Tell us how you will travel between campuses.';
-    return
-  }
-  
-  if (isPastDate(availableDate)) {
-    output.textContent = "Please choose a later date.";
-    return;
-  }
+  saveEmail(); // saves to Firestore
 
   output.innerHTML = `
-  <h2>Preference Submitted</h2>
-  <p>${firstName} ${lastName}</p>
-  <p>Email: ${email}</p>
-  <p>Availability: ${availableDate}</p>
-  <p>Preference Level: ${type}</p>
+    <h2>Contact Submitted</h2>
+    <p>${firstName} ${lastName}</p>
+    <p>Email: ${email}</p>
   `;
 
   form.reset();
-  updateNotesField();
 });
+
           
+
+
