0ed1b183294932759e4facfa815a4b388d5f71ea
Maxwell0613-user <traviscm0613@gmail.com>
2026-06-17 12:52:09 -0600

    worked on rolls

--- home.js
+++ home.js
@@ -1,24 +1,25 @@
-/*
-import { initializeApp } from "firebase/app";
-import{ getAuth} from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
-import { getAnalytics } from "firebase/analytics";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyClC6QLVoSF8aSeDr7gS7cwavNWLOCd-fU",
-    authDomain: "rocky-mountain-rolls.firebaseapp.com",
-    projectId: "rocky-mountain-rolls",
-    storageBucket: "rocky-mountain-rolls.firebasestorage.app",
-    messagingSenderId: "953087171248",
-    appId: "1:953087171248:web:5afa477880eb2cf0b2f975",
-    measurementId: "G-X2B2NCHSQ5"
+  apiKey: "AIzaSyClC6QLVoSF8aSeDr7gS7cwavNWLOCd-fU",
+  authDomain: "rocky-mountain-rolls.firebaseapp.com",
+  projectId: "rocky-mountain-rolls",
+  storageBucket: "rocky-mountain-rolls.firebasestorage.app",
+  messagingSenderId: "953087171248",
+  appId: "1:953087171248:web:5afa477880eb2cf0b2f975",
+  measurementId: "G-X2B2NCHSQ5"
 };
 
-// Initialize Firebase (CDN version)
-firebase.initializeApp(firebaseConfig);
-
-// Firestore reference
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 // ============================
@@ -55,7 +56,6 @@ function saveEmail() {
         });
 }
 
-*/
 
 /* form work */
 
